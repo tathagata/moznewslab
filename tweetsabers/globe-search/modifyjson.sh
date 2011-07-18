5496cc37b561fcc29865831a108c190f9bf27e0d
Tathagata Dasgupta <tathagatadg@gmail.com>
2011-07-17 23:46:50 -0500

    Just changing the directory structure

--- tweetsabers/globe-search/modifyjson.sh
+++
@@ -1,7 +0,0 @@
-
-#To understand the the data::
-#cat tweetdata.json.clean | tr ',' '\n' | awk 'BEGIN{i=0}{arr[i]=$0;i++;}END{for (i=2;i<NR;i+=3) c+=arr[i]; for(i=2;i<NR;i+=3)  print arr[i-2],",", arr[i-1], ","arr[i], ",",arr[i]}' | tr '\n' ',' > data.json
-
-cat tweetdata.json.clean | tr ',' '\n' | awk 'BEGIN{i=0}{arr[i]=$0;i++;}END{for (i=2;i<NR;i+=3) c+=arr[i]; for(i=2;i<NR;i+=3)  print arr[i-2],",", arr[i-1], ","arr[i]i/c, ",",arr[i]}' | tr '\n' ',' > data.json
-
-
